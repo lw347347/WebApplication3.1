09ea7f9cf10b5c54c7a3a1a9e174d9c1048577dc
Landon Williams <lw347347@byu.edu>
2019-10-09 19:16:26 -0600

    1. Added links for the CustomScript.js and CustomStylesheet.css 2. Added javaScript function to fade result away on index pages for stack, queue, and dictionary

--- WebApplication3/Scripts/CustomScript.js
+++ WebApplication3/Scripts/CustomScript.js
@@ -8,4 +8,19 @@ function w3_close() {
     document.getElementById("main").style.marginLeft = "0%";
     document.getElementById("mySidebar").style.display = "none";
     document.getElementById("openNav").style.display = "inline-block";
-}
+} 
+
+// This function allows the result to fade away
+setTimeout(function myTimer() {
+    var fadeTarget = document.getElementById("viewbagResult");
+    var fadeEffect = setInterval(function () {
+        if (!fadeTarget.style.opacity) {
+            fadeTarget.style.opacity = 1;
+        }
+        if (fadeTarget.style.opacity > 0) {
+            fadeTarget.style.opacity -= 0.1;
+        } else {
+            clearInterval(fadeEffect);
+        }
+    }, 200);
+}, 1000)
